ca6d6aaef25f3bf3fdfa02eb65b7804292974638
Paul Conte <paul.r.conte@gmail.com>
2018-10-17 13:13:55 -0700

    add About route

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, Switch } from 'react-router-dom';
 
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
@@ -53,20 +54,25 @@ class App extends Component {
             <section className="section">
                 <div className="container">
                     <div className="columns">
-                        <div className="column is-one-third">
+                        <div className="column is-half">
                             <br/>
-                            <h1 className="title is-1 is-1">All Users</h1>
-                            <hr/><br/>
-                            <AddUser
-                                username={this.state.username}
-                                email={this.state.email}
-                                addUser={this.addUser}
-                                handleChange={this.handleChange}
-                            />
-                            <br/><br/>
-                            <UsersList users={this.state.users}/>
-                            <br/>
-                            <About/>
+                            <Switch>
+                                <Route exact path='/' render={() => (
+                                    <div>
+                                        <h1 className="title is-1 is-1">All Users</h1>
+                                        <hr/><br/>
+                                        <AddUser
+                                            username={this.state.username}
+                                            email={this.state.email}
+                                            addUser={this.addUser}
+                                            handleChange={this.handleChange}
+                                        />
+                                        <br/><br/>
+                                        <UsersList users={this.state.users}/>
+                                    </div>
+                                )} />
+                                <Route exact path='/about' component={About}/>
+                            </Switch>
                         </div>
                     </div>
                 </div>
