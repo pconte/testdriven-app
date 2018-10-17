8c1866331adc95b8b346dbb8d5181484fccb9492
Paul Conte <paul.r.conte@gmail.com>
2018-10-17 13:31:17 -0700

    create NavBar component

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
 import About from './components/About';
+import NavBar from './components/NavBar';
 
 
 class App extends Component {
@@ -13,7 +14,8 @@ class App extends Component {
         this.state = {
             users: [],
             username: '',
-            email: ''
+            email: '',
+            title: 'TestDriven.io'
         };
         this.addUser = this.addUser.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -51,32 +53,35 @@ class App extends Component {
 
     render() {
         return (
-            <section className="section">
-                <div className="container">
-                    <div className="columns">
-                        <div className="column is-half">
-                            <br/>
-                            <Switch>
-                                <Route exact path='/' render={() => (
-                                    <div>
-                                        <h1 className="title is-1 is-1">All Users</h1>
-                                        <hr/><br/>
-                                        <AddUser
-                                            username={this.state.username}
-                                            email={this.state.email}
-                                            addUser={this.addUser}
-                                            handleChange={this.handleChange}
-                                        />
-                                        <br/><br/>
-                                        <UsersList users={this.state.users}/>
-                                    </div>
-                                )} />
-                                <Route exact path='/about' component={About}/>
-                            </Switch>
+            <div>
+                <NavBar title={this.state.title} />
+                <section className="section">
+                    <div className="container">
+                        <div className="columns">
+                            <div className="column is-half">
+                                <br/>
+                                <Switch>
+                                    <Route exact path='/' render={() => (
+                                        <div>
+                                            <h1 className="title is-1 is-1">All Users</h1>
+                                            <hr/><br/>
+                                            <AddUser
+                                                username={this.state.username}
+                                                email={this.state.email}
+                                                addUser={this.addUser}
+                                                handleChange={this.handleChange}
+                                            />
+                                            <br/><br/>
+                                            <UsersList users={this.state.users}/>
+                                        </div>
+                                    )} />
+                                    <Route exact path='/about' component={About}/>
+                                </Switch>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
         )
     }
 }
